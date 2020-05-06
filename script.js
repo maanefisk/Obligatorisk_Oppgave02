390c9ad27b68ab5b6c8b1b4953a49646902f6184
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-05-06 13:35:39 +0200

    Controller

---
+++ script.js
@@ -0,0 +1,45 @@
+// controller (ingenting her ennå)
+function calcColor(min, max, val) {
+    var minHue = 240, maxHue = 0;
+    var curPercent = (val - min) / (max - min);
+    var colString = "hsl(" + ((curPercent * (maxHue - minHue)) + minHue) + ",100%,50%)";
+    return colString;
+}
+
+function choosery(barNo,number) {
+    theHeight = number;
+    chosenBar = chosenBar == barNo ? null : barNo;
+    inputValue = theHeight;
+    stateOfButton()
+        show();
+}
+
+function addNewBar() {
+    checkValue();
+    var theInput = inputValue;
+    if (theInput > 0) { numbers.push(theInput); }
+    show();
+    
+}
+
+function removeBar() {
+    numbers.splice((chosenBar - 1), 1);
+    show();
+    }
+
+function changeABar() {
+    checkValue();
+    var thePosition = numbers.indexOf(theHeight)+1;
+    numbers[thePosition-1] = inputValue;
+    show();
+    }
+
+function stateOfButton() {
+    if (chosenBar != undefined) {state = 'enabled'}
+    else {state = undefined}
+    }
+
+function checkValue() {
+    if (inputValue > 10 || inputValue < 1){alert('Unvalid input number');}
+}
+
